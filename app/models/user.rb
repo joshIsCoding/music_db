14be23298065bc983e9541a380bdeab851d59829
joshIsCoding <yes_you_do@hotmail.com>
2019-12-27 12:50:28 +0000

    Add ::generate_session_token and #reset_session_token!

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,22 @@ class User < ApplicationRecord
 
    attr_reader :password
 
+   def self.generate_session_token
+      SecureRandom::urlsafe_base64(16)
+   end
+
    def password=(password)
       self.password_digest = BCrypt::Password.create(password)
    end
 
    def is_password?(password)
       BCrypt::Password.new(self.password_digest).is_password?(password)
+   end
+
+   def reset_session_token!
+      self.session_token = self.class.generate_session_token
+      self.save!
+      self.session_token
    end
 
    
