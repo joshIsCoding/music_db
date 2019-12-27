5073cc83228b8f7bc8eadac6c0fd2fc01377a814
joshIsCoding <yes_you_do@hotmail.com>
2019-12-27 23:13:09 +0000

    Add update action

--- app/controllers/bands_controller.rb
+++ app/controllers/bands_controller.rb
@@ -29,6 +29,16 @@ class BandsController < ApplicationController
       render :edit
    end
 
+   def update
+      @band = Band.find_by_id(params[:id])
+      if @band.update_attributes(band_params)
+         redirect_to band_url(@band)
+      else
+         flash.now[:errors] = @band.errors.full_messages
+         render :edit
+      end
+   end
+
    private
    def band_params
       params.require(:band).permit(:name)
