26be9ac54157cb9b99053550b0a048eb89c3d70d
joshIsCoding <yes_you_do@hotmail.com>
2019-12-27 22:06:48 +0000

    Add bands create action

--- app/controllers/bands_controller.rb
+++ app/controllers/bands_controller.rb
@@ -13,4 +13,19 @@ class BandsController < ApplicationController
       @band = Band.new
       render :new
    end 
+
+   def create
+      @band = Band.new(band_params)
+      if @band.save
+         redirect_to band_url(@band)
+      else
+         flash.now[:errors] = @band.errors.full_messages
+         render :new
+      end
+   end
+
+   private
+   def band_params
+      params.require(:band).permit(:name)
+   end
 end
