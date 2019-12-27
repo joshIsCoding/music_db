8861eec7429dbdba3847d392771d4c65f854938e
joshIsCoding <yes_you_do@hotmail.com>
2019-12-27 13:18:21 +0000

    Add User::find_by_credentials

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,12 @@ class User < ApplicationRecord
    
    attr_reader :password
 
+   def self.find_by_credentials(email, password)
+      user = User.find_by(email: email)
+      return user if user.is_password?(password)
+      nil
+   end
+
    def self.generate_session_token
       SecureRandom::urlsafe_base64(16)
    end
