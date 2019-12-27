350c860f4c12b7c97f75aa7e6c608b53bb2ca0f2
joshIsCoding <yes_you_do@hotmail.com>
2019-12-27 13:10:20 +0000

    Add #ensure_session_token and after_initialization validation thereof

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@ class User < ApplicationRecord
    validates :email, :session_token, :password_digest, presence: true
    validates :password, allow_nil: true, length: { minimum: 5 }
 
+   # checks pre-existence of session token for User instances that should already 
+   # be logged in, such as those used in HTTP requests
+   after_initialize :ensure_session_token
+   
    attr_reader :password
 
    def self.generate_session_token
@@ -22,6 +26,10 @@ class User < ApplicationRecord
       self.session_token
    end
 
-   
+   private
+
+   def ensure_session_token
+      self.session_token ||= self.class.generate_session_token
+   end
    
 end
