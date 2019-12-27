7718413a1f18af96735646b51382e0416fd49bc2
joshIsCoding <yes_you_do@hotmail.com>
2019-12-27 20:00:30 +0000

    Add SessionsController with Create, New and Destroy actions

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,27 @@
+class SessionsController < ApplicationController
+   def create
+      user = User.find_by_credentials(params[:user][:email], params[:user][:password])
+      if user
+         login_user!(user)
+         render "users/show"
+      else
+         flash.now[:errors] ||= []
+         flash.now[:errors] << "Those were invalid user credentials. Please try again"
+         render :new
+      end
+   end
+
+   def new
+      render :new
+   end
+
+   def destroy
+      if current_user
+         current_user.reset_session_token!
+         session_token[:session_token] = nil
+      end
+      redirect_to new_session_url
+   end
+
+
+end
