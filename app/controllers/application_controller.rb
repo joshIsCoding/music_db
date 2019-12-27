29edbf6b4b6ea7cb72e4baba002efe32c9785160
joshIsCoding <yes_you_do@hotmail.com>
2019-12-27 19:58:48 +0000

    Add shared methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+   def current_user
+      return nil unless session[:session_token]
+      @current_user ||= User.find_by(session[:session_token])
+   end
+
+   def logged_in?
+      current_user.exists?
+   end
+
+   def log_in_user!(user)
+      session[:session_token] = user.reset_session_token!
+      nil
+   end
 end
