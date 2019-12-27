1be5fdfae64011b60cc5b5ec7594f0487c9f8a00
joshIsCoding <yes_you_do@hotmail.com>
2019-12-27 21:25:06 +0000

    Add show BandsController show action and corresponding view

--- app/controllers/bands_controller.rb
+++ app/controllers/bands_controller.rb
@@ -1,7 +1,11 @@
 class BandsController < ApplicationController
    def index
-      @bands = Band.all
+      @bands = Band.all.order(:name)
       render :index
    end
 
+   def show
+      @band = Band.find_by_id(params[:id])
+      render :show
+   end
 end
