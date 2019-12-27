1a3aea44a0003e8b952c65737f220c68e53db0c6
joshIsCoding <yes_you_do@hotmail.com>
2019-12-27 12:49:38 +0000

    Add #password= and #is_password? methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,6 @@ class User < ApplicationRecord
 
    attr_reader :password
 
-   def self.generate_session_token
-      SecureRandom::urlsafe_base64(16)
-   end
-
    def password=(password)
       self.password_digest = BCrypt::Password.create(password)
    end
@@ -16,10 +12,6 @@ class User < ApplicationRecord
       BCrypt::Password.new(self.password_digest).is_password?(password)
    end
 
-   def reset_session_token!
-      self.session_token = self.class.generate_session_token
-      self.save!
-      self.session_token
-   end
+   
    
 end
