64da22ac54cbc4bd3851264ff4e345bce10fc299
joshIsCoding <yes_you_do@hotmail.com>
2019-12-28 18:30:33 +0000

    Generate and run migration to rename 'bands' to 'artists'

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_28_000201) do
+ActiveRecord::Schema.define(version: 2019_12_28_182857) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,7 +27,7 @@ ActiveRecord::Schema.define(version: 2019_12_28_000201) do
     t.index ["title", "band_id"], name: "index_albums_on_title_and_band_id", unique: true
   end
 
-  create_table "bands", force: :cascade do |t|
+  create_table "artists", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
