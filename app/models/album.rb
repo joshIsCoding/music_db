99853f0396986eaad015ad76b1fdb8407f92b36f
joshIsCoding <yes_you_do@hotmail.com>
2019-12-28 00:35:39 +0000

    Add #artist validation

--- app/models/album.rb
+++ app/models/album.rb
@@ -3,4 +3,10 @@ class Album < ApplicationRecord
    validates :album_type, inclusion: { in: %w(EP LP Compilation) }
    validates :title, uniqueness: {scope: :band_id }
 
+   belongs_to(
+      :artist,
+      class_name: "Band",
+      foreign_key: :band_id,
+      primary_key: :id
+   )
 end
