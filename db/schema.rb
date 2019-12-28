04c59a3f4c627548751e5df2cbb7761497bf88e4
joshIsCoding <yes_you_do@hotmail.com>
2019-12-28 00:33:35 +0000

    Generate and run migration to create albums table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_27_201602) do
+ActiveRecord::Schema.define(version: 2019_12_28_000201) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "albums", force: :cascade do |t|
+    t.string "title", null: false
+    t.integer "band_id", null: false
+    t.integer "year", null: false
+    t.string "album_type", null: false
+    t.boolean "live?", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["band_id"], name: "index_albums_on_band_id"
+    t.index ["title", "band_id"], name: "index_albums_on_title_and_band_id", unique: true
+  end
 
   create_table "bands", force: :cascade do |t|
     t.string "name", null: false
