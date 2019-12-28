fd1496249a70df88db76c6b6c89559ffe97a8880
joshIsCoding <yes_you_do@hotmail.com>
2019-12-28 21:53:32 +0000

    Add create action for albums

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -10,4 +10,19 @@ class AlbumsController < ApplicationController
       @artists = Artist.all
       render :new
    end
+
+   def create
+      @album = Album.new(album_params)
+      if @album.save
+         redirect_to album_url(@album)
+      else
+         flash.now[:errors] = @album.errors.full_messages
+         render :new
+      end
+   end
+
+   private
+   def album_params
+      params.require(:album).permit(:title, :artist_id, :year, :album_type, :live?)
+   end
 end
