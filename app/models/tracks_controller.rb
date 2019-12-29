91b2052ada3d6167205d56003515ab9a596941b9
joshIsCoding <yes_you_do@hotmail.com>
2019-12-29 15:48:53 +0000

    Add create action

--- app/models/tracks_controller.rb
+++ app/models/tracks_controller.rb
@@ -8,4 +8,20 @@ class TracksController < ApplicationController
       @track = Track.new(album_id: params[:album_id])
       render :new
    end
+
+
+   def create
+      @track = Track.new(track_params)
+      if @track.save
+         redirect_to track_url(@track)
+      else
+         flash.now[:errors] = @track.errors.full_messages
+         render :new
+      end
+   end
+
+   private
+   def track_params
+      params.require(:track).permit(:title, :album_id, :ord, :lyrics, :bonus?)
+   end
 end
