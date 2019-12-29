145c4b663cbcbbfa19aeedbb42c202e754055d18
joshIsCoding <yes_you_do@hotmail.com>
2019-12-29 21:10:32 +0000

    Fix typo in destroy action where 'session' was incorrectly 'session_token'

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -18,7 +18,7 @@ class SessionsController < ApplicationController
    def destroy
       if current_user
          current_user.reset_session_token!
-         session_token[:session_token] = nil
+         session[:session_token] = nil
       end
       redirect_to new_session_url
    end
