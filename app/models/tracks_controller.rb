660f82d2ef19fb668bf81b6b5a81400f76830878
joshIsCoding <yes_you_do@hotmail.com>
2019-12-29 15:31:17 +0000

    Add 'new' action and corresponding view

--- app/models/tracks_controller.rb
+++ app/models/tracks_controller.rb
@@ -3,4 +3,9 @@ class TracksController < ApplicationController
       @track = Track.find_by_id(params[:id])
       render :show
    end
+
+   def new
+      @track = Track.new(album_id: params[:album_id])
+      render :new
+   end
 end
