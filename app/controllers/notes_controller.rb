a57050c8a0aa3f613491e8bdbb3b888873c7ce98
joshIsCoding <yes_you_do@hotmail.com>
2019-12-30 13:38:38 +0000

    Add before 'destroy'  action filter to ensure the user owns the note they wish to delete

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,4 +1,5 @@
 class NotesController < ApplicationController
+   before_action :user_owns_note, only: [:destroy]
    def create
       @note = Note.new(params.require(:note).permit(:content, :track_id))
       @note.user = current_user      
@@ -7,9 +8,18 @@ class NotesController < ApplicationController
    end
 
    def destroy
-      @note = Note.find_by_id(params[:id])
+      @note ||= Note.find_by_id(params[:id])
       @note.destroy
       redirect_to track_url(@note.track)
    end
+
+   private
+   def user_owns_note
+      @note = Note.find_by_id(params[:id])
+      unless @note.user_id == current_user.id
+         render plain: "You cannot delete other users' notes!", status: :forbidden
+      end
+   end
+
 
 end
