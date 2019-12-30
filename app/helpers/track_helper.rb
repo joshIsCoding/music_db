ba7f3c7acf3f22ec6819f45933df702093c9a425
joshIsCoding <yes_you_do@hotmail.com>
2019-12-30 15:08:16 +0000

    Tweak lyric_trim to use a single #match? regexp, to substitute semi-colons for any linebreaks and to use an optional length argument

--- app/helpers/track_helper.rb
+++ app/helpers/track_helper.rb
@@ -1,11 +1,14 @@
 module TrackHelper
-   def lyric_trim(lyrics)
-      trimmed = lyrics[0..25]
-      if trimmed.length > 25
-         trimmed.chop! until trimmed.last.match?(/\w/)
+   def lyric_trim(lyrics, length=30)
+      trimmed = lyrics[0..length]
+      # use semi-colons to signify linebreaks in snippet preview
+      trimmed.gsub!(/(\n|\r|\r\n)+/, "; ")
+      if trimmed.length > length
+         # trims until a word character is found
+         trimmed.chop! until trimmed.match?(/.*?\w$/)
          trimmed += "..."
       end
       trimmed
    end
-   
+
 end
