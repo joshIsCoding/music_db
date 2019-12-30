2a5084a80e47f09bae86cec545d85ba7a4580cf0
joshIsCoding <yes_you_do@hotmail.com>
2019-12-30 17:52:47 +0000

    Alter regexp in lyric_trim to handle pre-line-break punctuation and spacing

--- app/helpers/track_helper.rb
+++ app/helpers/track_helper.rb
@@ -2,7 +2,8 @@ module TrackHelper
    def lyric_trim(lyrics, length=30)
       trimmed = lyrics[0..length]
       # use semi-colons to signify linebreaks in snippet preview
-      trimmed.gsub!(/(\n|\r|\r\n)+/, "; ")
+      # trim any pre-break punctuation or rogue spacing
+      trimmed.gsub!(/\W*\s*(\n|\r|\r\n)+/, "; ")
       if trimmed.length > length
          # trims until a word character is found
          trimmed.chop! until trimmed.match?(/.*?\w$/)
