61b9688d4058c77dcc4e918e0e1ba1dc1d0db4c9
joshIsCoding <yes_you_do@hotmail.com>
2019-12-30 21:07:49 +0000

    Add custom activate_users collection route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users, only: [:create, :new]
+  resources :users, only: [:create, :new] do
+    get 'activate', on: :collection
+  end
   resource :session, only: [:create, :new, :destroy]
   
   # assets
