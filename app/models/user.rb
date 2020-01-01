290570ab6d854fbb2be1121f699cd678e0888270
joshIsCoding <yes_you_do@hotmail.com>
2020-01-01 01:09:20 +0000

    Change if-condition in ::find_by_credentials to prevent unintended nill-class exception being raised

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ApplicationRecord
 
    def self.find_by_credentials(email, password)
       user = User.find_by(email: email)
-      return user if user.is_password?(password)
+      return user if user && user.is_password?(password)
       nil
    end
 
